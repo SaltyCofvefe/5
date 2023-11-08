b87e928e09c489c99aadf1cae84ad3bee42ae918
Jesse Puustinen <116886159+SaltyCofvefe@users.noreply.github.com>
2023-11-09 00:55:51 +0200

    Add files via upload

--- controllers/postControllers.js
+++ controllers/postControllers.js
@@ -1,14 +1,92 @@
+const Post = require('../models/Post')
+
 exports.getAllPosts = async (req, res, next) => {
-    console.log("Inside getAllPosts handler");
-    res.send("Get all posts route");
+    try{
+        const [posts, _] = await Post.findAll();
+
+        res.status(200).json({posts});
+    } catch (error) {
+        console.log(error);
+        next(error);
+
+
+    }
   }
   
   exports.createNewPosts = async (req, res, next) => {
-    console.log("Inside createNewPosts handler");
-    res.send("Create new post routes");
+try {
+    let {title, body} = req.body;
+    let post = new Post(title, body);
+
+    post = await post.save();
+
+    res.status(201).json({ message: "Post created"})
+    
+} catch (error) {
+    console.log(error);
+        next(error);
+}
   }
   
   exports.getPostById = async (req, res, next) => {
-    console.log("Inside getPostById handler");
-    res.send("Get Post By ID Route");
+    try {
+        let postId = req.params.id;
+
+        let [post, _]= await Post.findById(postId) ;
+
+        res.status(200).json({post: post[0]});
+    } catch (error) {
+        console.log(error);
+        next(error); 
+    }
+    
   }
+  exports.updatePost = async (req, res, next) => {
+    try {
+        const postId = req.params.id;
+        const { title, body } = req.body;
+        
+        const [post, _] = await Post.update(postId, { title, body });
+
+        if (post) {
+            res.status(200).json({ message: "Post updated" });
+        } else {
+            res.status(404).json({ message: "Post not found" });
+        }
+    } catch (error) {
+        console.log(error);
+        next(error);
+    }
+}
+
+
+exports.updatePost = async (req, res, next) => {
+    try {
+        const postId = req.params.id;
+        const { title, body } = req.body;
+        
+        
+
+        const updatedPost = await Post.update(postId, title, body);
+
+        res.status(200).json({ message: "Post updated", updatedPost });
+    } catch (error) {
+        console.log(error);
+        next(error);
+    }
+}
+
+exports.deletePost = async (req, res, next) => {
+    try {
+        const postId = req.params.id;
+
+        
+
+        await Post.delete(postId);
+
+        res.status(200).json({ message: "Post deleted" });
+    } catch (error) {
+        console.log(error);
+        next(error);
+    }
+}
